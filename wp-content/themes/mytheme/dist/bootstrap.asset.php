797b5a758a24166fe136e2338481e3ef30243437
kjroelke <kj.roelke@gmail.com>
2023-10-21 10:10:26 -0500

    fix autoloading and infinite loop

---
+++ wp-content/themes/mytheme/dist/bootstrap.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '914cc316a7b8552b1c1e');
---
+++ wp-content/themes/mytheme/src/js/vendors/bootstrap.js
@@ -0,0 +1,2 @@
+import '../../styles/vendors/bootstrap-custom.scss';
+import Offcanvas from 'bootstrap/js/dist/offcanvas';
